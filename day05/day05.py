c9a8365e935b2dd7dbd24c4d13f7b79fac8217f8
Douglas Valentine <douglasvalentine@Douglass-MacBook-Pro.local>
2020-12-04 22:42:33 -0900

    day 5 first half

---
+++ day05/day05.py
@@ -0,0 +1,41 @@
+test = 'BFFFBBFRRR' #row 70, column 7, seat ID 567
+
+def row_finder(code):
+    rows = list(range(0,128))
+    for idx in range(7):
+        divider = len(rows) // 2
+        if code[idx] == 'F':
+            rows = rows[:divider]
+        elif code[idx] == 'B':
+            rows = rows[divider:]
+        else:
+            print(f'Got a {code[idx]} instead of an F or B')
+    return rows[0]
+
+def col_finder(code):
+    cols = list(range(8))
+    for idx in range(7,10):
+        divider = len(cols) // 2
+        if code[idx] == 'L':
+            cols = cols[:divider]
+        elif code[idx] == 'R':
+            cols = cols[divider:]
+        else:
+            print(f'Got a {code[idx]} instead of an L or R')
+    return cols[0]
+
+def seat_id(code):
+    return row_finder(code) * 8 + col_finder(code)
+
+def highest_seat_id(input):
+    seat_ids = [seat_id(code) for code in input.split()]
+    return max(seat_ids)
+
+assert row_finder(test) == 70
+assert col_finder(test) == 7
+assert seat_id(test) == 567
+
+with open('input05.txt') as data:
+    inputs = data.read()
+
+print(highest_seat_id(inputs))
